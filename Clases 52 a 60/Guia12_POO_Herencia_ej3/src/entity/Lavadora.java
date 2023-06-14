f3cddc791628f5ec66afa9f662b04c9463e6ad2b
Maxi Sternhein <maxi0062009@hotmail.com>
2023-06-14 15:53:57 -0300

    carga ej 3 guia12

---
+++ Clases 52 a 60/Guia12_POO_Herencia_ej3/src/entity/Lavadora.java
@@ -0,0 +1,71 @@
+/*
+Se debe crear también una subclase llamada Televisor con los siguientes atributos:
+resolución (en pulgadas) y sintonizador TDT (booleano), además de los atributos
+heredados.
+Los constructores que se implementarán serán:
+• Un constructor vacío.
+• Un constructor con la resolución, sintonizador TDT y el resto de los atributos
+heredados. Recuerda que debes llamar al constructor de la clase padre.
+Los métodos que se implementara serán:
+• Método get y set de los atributos resolución y sintonizador TDT.
+• Método crearTelevisor(): este método llama a crearElectrodomestico() de la clase
+padre, lo utilizamos para llenar los atributos heredados del padre y después llenamos
+los atributos del televisor.
+• Método precioFinal(): este método será heredado y se le sumará la siguiente
+funcionalidad. Si el televisor tiene una resolución mayor de 40 pulgadas, se
+incrementará el precio un 30% y si tiene un sintonizador TDT incorporado, aumentará
+$500. Recuerda que las condiciones que hemos visto en la clase Electrodomestico
+también deben afectar al precio.
+Finalmente, en el main debemos realizar lo siguiente:
+Vamos a crear una Lavadora y un Televisor y llamar a los métodos necesarios para mostrar
+el precio final de los dos electrodomésticos.
+ */
+package entity;
+
+/**
+ *
+ * @author Max
+ */
+public class Lavadora extends Electrodomestico {
+
+    private double carga;
+
+    /*
+    A continuación, se debe crear una subclase llamada Lavadora, con el atributo carga,
+además de los atributos heredados.
+Los constructores que se implementarán serán:
+• Un constructor vacío.
+• Un constructor con la carga y el resto de los atributos heredados. Recuerda que debes
+llamar al constructor de la clase padre.
+Los métodos que se implementara serán:
+• Método get y set del atributo carga.
+     */
+    public Lavadora() {
+    }
+
+    public Lavadora(double carga) {
+        this.carga = carga;
+    }
+
+    public Lavadora(double carga, double precio, String color, char consumoEnergetico, double peso) {
+        super(precio, color, consumoEnergetico, peso);
+        this.carga = carga;
+    }
+
+    public double getCarga() {
+        return carga;
+    }
+
+    public void setCarga(double carga) {
+        this.carga = carga;
+    }
+
+    @Override
+public String toString() {
+    return "Lavadora{" +
+            super.toString() +
+            ", carga=" + carga + "Kg" +
+            '}';
+}
+
+}
---
+++ Clases 52 a 60/Guia12_POO_Herencia_ej3/src/main/Main.java
@@ -0,0 +1,70 @@
+/*
+3. Siguiendo el ejercicio anterior, en el main vamos a crear un ArrayList de Electrodomésticos
+para guardar 4 electrodomésticos, ya sean lavadoras o televisores, con valores ya asignados.
+Luego, recorrer este array y ejecutar el método precioFinal() en cada electrodoméstico. Se
+deberá también mostrar el precio de cada tipo de objeto, es decir, el precio de todos los
+televisores y el de las lavadoras. Una vez hecho eso, también deberemos mostrar, la suma del
+precio de todos los Electrodomésticos. Por ejemplo, si tenemos una lavadora con un precio de
+2000 y un televisor de 5000, el resultado final será de 7000 (2000+5000) para
+electrodomésticos, 2000 para lavadora y 5000 para televisor.
+ */
+package main;
+
+import entity.Electrodomestico;
+import entity.Lavadora;
+import entity.Televisor;
+import java.util.ArrayList;
+import service.ElectrodomesticoService;
+
+/**
+ *
+ * @author Max
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        vamos a crear un ArrayList de Electrodomésticos
+para guardar 4 electrodomésticos, ya sean lavadoras o televisores, con valores ya asignados.
+         */
+        ArrayList<Electrodomestico> electro = new ArrayList();
+        electro.add(new Lavadora(15d, 2500d, "Blanco", 'D', 50d));
+        electro.add(new Lavadora(18d, 3700d, "Gris", 'A', 32d));
+        electro.add(new Televisor(55, false, 2500d, "Negro", 'B', 10d));
+        electro.add(new Televisor(75, true, 6500d, "Negro", 'A', 18d));
+
+        /*
+        Luego, recorrer este array y ejecutar el método precioFinal() en cada electrodoméstico. Se
+deberá también mostrar el precio de cada tipo de objeto, es decir, el precio de todos los
+televisores y el de las lavadoras.
+         */
+        ElectrodomesticoService electrodomesticoServ = new ElectrodomesticoService();
+        double precioTotal = 0;
+        double precioLavadoras = 0;
+        double precioTelevisores = 0;
+        for (Electrodomestico electrodomestico : electro) {
+            //System.out.println("Caracteristicas del electrodoméstico:");
+            electrodomesticoServ.precioFinal(electrodomestico);
+            precioTotal += electrodomestico.getPrecio();
+            if (electrodomestico instanceof Lavadora) {
+                precioLavadoras += electrodomestico.getPrecio();
+            } else if (electrodomestico instanceof Televisor) {
+                precioTelevisores += electrodomestico.getPrecio();
+            }
+            /*
+    si no coloco el "instanceof" cada vez q llamo a getPrecio los precios se ma cargaban en un
+    solo objeto indistinto de que sea lavadora o tele, con esto en cada iteracion comprueba hacia 
+    que objeto va dirigido el getPrecio 
+             */
+
+            System.out.println(electrodomestico.toString());
+            System.out.println("------------------------------");
+        }
+
+        System.out.println("Precio por objetos:");
+        System.out.println("Precio de las lavadoras: " + precioLavadoras);
+        System.out.println("Precio de los televisores: " + precioTelevisores);
+        System.out.println("------------------------------");
+        System.out.println("Precio total de todos los electrodomésticos: " + precioTotal);
+    }
+}
